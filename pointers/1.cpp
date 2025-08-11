4e1ccff7d87261a189b06e3cc8507bd8b33025bc
Rana Aishwarya Pratap Singh <67787691+rapsingh@users.noreply.github.com>
2025-08-12 00:33:59 +0530

    char and int pointers difference of size in actual code

--- pointers/1.cpp
+++ pointers/1.cpp
@@ -11,6 +11,15 @@ a=1025;
 p=&a;
 printf("address of a %d\n",p);
 printf("address of p itself %d\n",&p);
+printf("sizeof of int %d\n",sizeof(int));
+printf("value of p + 1 is: %d\n",p+1);
+printf("value at p + 1 is: %d\n",*(p+1));
+printf("value at p  is: %d\n",*p);
 
-    return 0;
+char* p0;
+p0 = (char*)p;
+printf("value at p0  is: %d\n",*p0);
+printf("address of p0 is: %d\n",*p0);
+printf("value at p0 + 1 is: %d\n",*(p0+1));
+ return 0;
 }
